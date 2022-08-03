ebb2aabf5a7c2ea0315f73bb3e6b106c33a8a598
YeHaobo <17855366369@163.com>
2022-08-03 11:21:33 +0800

    更新2022.08.03

--- TestWebview/app/src/main/java/com/yhb/testwebview/WebviewFragment.java
+++ TestWebview/app/src/main/java/com/yhb/testwebview/WebviewFragment.java
@@ -18,22 +18,22 @@ public class WebviewFragment extends BaseWebviewFragment {
 
     @Override
     public String getOnResumeFunctionName() {
-        return "onWebResume";
+        return "onWebResume";//当fragment进入OnResume时会调用JS中onWebResume方法
     }
 
     @Override
     public String getOnPauseFunctionName() {
-        return "onWebPause";
+        return "onWebPause";//当fragment进入OnPause时会调用JS中onWebPause方法
     }
 
     @Override
     public String getOnStopFunctionName() {
-        return "onWebStop";
+        return "onWebStop";//当fragment进入OnStop时会调用JS中onWebStop方法
     }
 
     @Override
     public String getOnDestroyFunctionName() {
-        return "onWebDestroy";
+        return "onWebDestroy";//当fragment进入OnDestroy时会调用JS中onWebDestroy方法
     }
 
 }
